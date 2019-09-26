fa5bc0a53a63f6ce9869a4d941766a97a72b4fc8
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-26 16:54:35 +0800

    fix

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,7 @@
 sed -i "s/cpu/nvidia/g" ./config/.faceswap
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker-gpu:$1 -f docker/docker_faceswap_gpu/Dockerfile .
 sed -i "s/nvidia/cpu/g" ./config/.faceswap
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker:$1 -f docker/docker_yanqing/Dockerfile .
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker:$1 -f docker/docker_faceswap/Dockerfile .
 
 
 docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker:$1
