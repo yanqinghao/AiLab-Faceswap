e34f769da885d180260c1acc14cd3630d2968cb2
YanQinghao <woshiyanqinghao@gmail.com>
2019-10-15 15:14:56 +0800

    change dir

--- entrypoint_convert.sh
+++ entrypoint_convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Edit Mode"
-cp /yanqing/convert.sh /run/run.sh
-chmod +x /run/run.sh
+cp /yanqing/convert.sh /workspace/run.sh
+chmod +x /workspace/run.sh
 jupyter notebook --NotebookApp.port=8888 --NotebookApp.password_required=False --NotebookApp.base_url=/proxy/${SP_USER_ID}/${SP_APP_ID}/${SP_NODE_ID}/8888/ --NotebookApp.open_browser=False --NotebookApp.ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.notebook_dir=${PWD} --allow-root
