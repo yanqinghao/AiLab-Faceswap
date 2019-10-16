26ec267fee11d90d8e435df04998026cc849d9ed
YanQinghao <woshiyanqinghao@gmail.com>
2019-10-16 17:38:06 +0800

    fix save file

--- components/docker/SPConvertSync.py
+++ components/docker/SPConvertSync.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
+import os
 from suanpan.app.arguments import String
 from suanpan.app import app
 from suanpan.storage import storage
@@ -9,10 +10,13 @@ from suanpan.storage import storage
 @app.output(String(key="outputData"))
 def SPConvertSync(context):
     args = context.args
-    storage.upload(args.modelDir, "/sp_data/" + args.outputData)
+    path = os.path.split(str(args.outputData))[0]
 
-    return "/sp_data/" + args.outputData
+    storage.upload(path.replace("/sp_data/", ""), path)
+
+    return "Images Saved..."
 
 
 if __name__ == "__main__":
     SPConvertSync()
+
--- components/docker/SPTrainSync.py
+++ components/docker/SPTrainSync.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
+import os
 from suanpan.app.arguments import String
 from suanpan.app import app
 from suanpan.storage import storage
@@ -9,9 +10,11 @@ from suanpan.storage import storage
 @app.output(String(key="modelDir"))
 def SPTrainSync(context):
     args = context.args
-    storage.upload(args.modelDir, "/sp_data/" + args.modelDir)
+    path = os.path.split(str(args.modelDir))[0]
 
-    return "/sp_data/" + args.modelDir
+    storage.upload(path.replace("/sp_data/", ""), path)
+
+    return "Model Saved..."
 
 
 if __name__ == "__main__":
