c8093ffcff50c421b0578d1f298437fd9ac0d88b
YanQinghao <woshiyanqinghao@gmail.com>
2019-10-16 14:20:42 +0800

    fix sync

--- components/docker/SPConvertSync.py
+++ components/docker/SPConvertSync.py
@@ -1,15 +1,17 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
-from suanpan.app.arguments import Folder
+from suanpan.app.arguments import String
 from suanpan.app import app
+from suanpan.storage import storage
 
 
-@app.output(Folder(key="outputData"))
+@app.output(String(key="outputData"))
 def SPConvertSync(context):
     args = context.args
+    storage.upload(args.modelDir, "/sp_data/" + args.outputData)
 
-    return args.outputData
+    return "/sp_data/" + args.outputData
 
 
 if __name__ == "__main__":
--- components/docker/SPTrainSync.py
+++ components/docker/SPTrainSync.py
@@ -1,15 +1,17 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
-from suanpan.app.arguments import Folder
+from suanpan.app.arguments import String
 from suanpan.app import app
+from suanpan.storage import storage
 
 
-@app.output(Folder(key="modelDir"))
+@app.output(String(key="modelDir"))
 def SPTrainSync(context):
     args = context.args
+    storage.upload(args.modelDir, "/sp_data/" + args.modelDir)
 
-    return args.modelDir
+    return "/sp_data/" + args.modelDir
 
 
 if __name__ == "__main__":
