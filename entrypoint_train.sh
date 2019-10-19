fba87edd444890a1edf42fbc25d6496e47f47470
YanQinghao <woshiyanqinghao@gmail.com>
2019-10-19 10:02:52 +0800

    fix

--- entrypoint_train.sh
+++ entrypoint_train.sh
@@ -30,5 +30,5 @@ then
     jupyter notebook --NotebookApp.port=8888 --NotebookApp.password_required=False --NotebookApp.base_url=/proxy/${SP_USER_ID}/${SP_APP_ID}/${SP_NODE_ID}/8888/ --NotebookApp.open_browser=False --NotebookApp.ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.notebook_dir=${PWD} --allow-root
 else
     echo "Run Mode"
-    bash -c "${SP_DISTRIBUTED_CMD} python run.py components.docker.SPTrainLab.SPTrainLab"
+    bash -c "${SP_DISTRIBUTED_CMD} python /yanqing/run.py components.docker.SPTrainLab.SPTrainLab"
 fi
