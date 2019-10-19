9a9d48df5b488fc867d9546fece53c418f5d5780
YanQinghao <woshiyanqinghao@gmail.com>
2019-10-19 09:16:05 +0800

    add edit mode

--- components/docker/SPConvertLab.py
+++ components/docker/SPConvertLab.py
@@ -1,14 +1,16 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
+import os
 from suanpan.app.arguments import Folder, String, Int, Bool
 from suanpan.app import app
-from mainscripts import Converter
+
 
 
 @app.input(Folder(key="inputData"))
 @app.input(Folder(key="alignedDir"))
 @app.input(Folder(key="modelDir"))
+@app.param(Bool(key="__edit", default=False))
 @app.param(
     String(
         key="modelName",
@@ -33,7 +35,9 @@ def SPConvertLab(context):
         "cpu_only": args.cpuOnly,
         "force_gpu_idx": args.forceGpuIdx,
     }
-
+    if not args.__edit:
+        os.environ["SP_FaceLab_Edit"] = "False"
+    from mainscripts import Converter
     Converter.main(convert_args, device_args)
 
     return args.outputData
