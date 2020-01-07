9306c5a06113008aa5b7a43ab2662be2936d3fe2
YanQinghao <woshiyanqinghao@gmail.com>
2020-01-07 14:24:53 +0800

    change namespace to shuzhi-amd64

--- tools/build.sh
+++ tools/build.sh
@@ -1,8 +1,12 @@
-sed -i "s/cpu/nvidia/g" ./config/.faceswap
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker-gpu:$1 -f docker/docker_faceswap_gpu/Dockerfile .
-sed -i "s/nvidia/cpu/g" ./config/.faceswap
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker:$1 -f docker/docker_faceswap/Dockerfile .
+NAMESPACE=("shuzhi-amd64")
+for i in ${NAMESPACE[*]}
+do
+    sed -i "s/cpu/nvidia/g" ./config/.faceswap
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/facelab-docker-gpu:$1 -f docker/docker_faceswap_gpu/Dockerfile .
+    sed -i "s/nvidia/cpu/g" ./config/.faceswap
+    docker build --build-arg NAME_SPACE=${i} -t registry-vpc.cn-shanghai.aliyuncs.com/${i}/facelab-docker:$1 -f docker/docker_faceswap/Dockerfile .
 
 
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/facelab-docker-gpu:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/facelab-docker:$1
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${i}/facelab-docker-gpu:$1
+done
